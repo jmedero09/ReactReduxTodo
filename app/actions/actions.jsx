022a3be339704b0ad6dd45c19b7f2a708c247b3c
Jesus Medero <jmedero09@gmail.com>
2016-12-23 13:31:53 -0500

    first commit putting redux and react together

--- app/actions/actions.jsx
+++ app/actions/actions.jsx
@@ -1,26 +1,26 @@
-export var setSearchText = (searchText) => {
-  return {
-    type: 'SET_SEARCH_TEXT',
-    searchText
-  };
+export var setSearchText = (searchText)=>{
+	return{
+		type:'SET_SEARCH_TEXT',
+		searchText:searchText
+	};
 };
 
-export var toggleShowCompleted = () => {
-  return {
-    type: 'TOGGLE_SHOW_COMPLETED'
-  };
-};
+export var addTodo = (text)=>{
+	return{
+		type:'ADD_TODO',
+		text:text
+	}
+}
 
-export var addTodo = (text) => {
-  return {
-    type: 'ADD_TODO',
-    text
-  };
-};
+export var toggleShowCompleted = ()=>{
+	return{
+		type:'TOGGLE_SHOW_COMPLETED'
+	}
+}
 
-export var toggleTodo = (id) => {
-  return {
-    type: 'TOGGLE_TODO',
-    id
-  };
-};
+export var toggleTodo = (id)=>{
+	return {
+		type:'TOGGLE_TODO',
+		id:id
+	}
+}
--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -1,43 +1,62 @@
 var expect = require('expect');
-var actions = require('actions');
+var actions = require('actions');//have to refrence by calling action.nameofactiongenerator
 
-describe('Actions', () => {
-  it('should generate search text action', () => {
+describe('Actions',()=>{
+  it('should generate search text action',()=>{
     var action = {
-      type: 'SET_SEARCH_TEXT',
-      searchText: 'Some search text'
+      type:'SET_SEARCH_TEXT',
+      searchText:'some search text'
     };
+    //The argument getting passed to the searchTextAction 
+    //generator is some search text which is define above 
     var res = actions.setSearchText(action.searchText);
 
     expect(res).toEqual(action);
+
   });
-
-  it('should generate toggle show completed action', () => {
+  it('should generate add todo action',()=>{
     var action = {
-      type: 'TOGGLE_SHOW_COMPLETED'
-    };
-    var res = actions.toggleShowCompleted();
-
-    expect(res).toEqual(action);
-  });
-
-  it('should generate add todo action', () => {
-    var action = {
-      type: 'ADD_TODO',
-      text: 'Thing to do'
-    };
+      type:'ADD_TODO',
+      text:'thing to do'
+    }
     var res = actions.addTodo(action.text);
+    expect(res).toEqual(action);
 
+  });
+
+  it('shuold generate toggleshowcompleted action',()=>{
+    var action = {
+      type:'TOGGLE_SHOW_COMPLETED'
+    }
+
+    var res = actions.toggleShowCompleted();
     expect(res).toEqual(action);
   });
 
-  it('should generate toggle todo action', () => {
+  it('shuold generate toggleTodo action',()=>{
     var action = {
-      type: 'TOGGLE_TODO',
-      id: '123'
-    };
+      type:'TOGGLE_TODO',
+      id:'9'
+    }
     var res = actions.toggleTodo(action.id);
-
     expect(res).toEqual(action);
   });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
